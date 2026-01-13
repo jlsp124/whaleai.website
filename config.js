482ea061bcc27840b6d9b7c3676c4544c2846a7b
Jovan Pahal <pahalj@outlook.com>
2026-01-12 20:58:17 -0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-﻿window.WHALEAI_CONFIG = {
-  WEBSITE_BASE_URL: "https://YOUR_GITHUB_PAGES_URL",
-  WORKER_API_BASE_URL: "https://YOUR_WORKER_URL",
-  TELEGRAM_URL: "https://t.me/YOUR_TELEGRAM",
-  DONATE_SOL_ADDRESS: "YOUR_SOL_ADDRESS",
-  SUPPORT_EMAIL: "support@YOUR_DOMAIN",
-  GITHUB_URL: "https://github.com/YOUR_REPO",
-  QR_IMAGE_BASE_URL: "https://api.qrserver.com/v1/create-qr-code/"
-};
